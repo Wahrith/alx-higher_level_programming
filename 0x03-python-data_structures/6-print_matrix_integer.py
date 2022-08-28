9aa6e18859ecac74040114e3aac5b6606491b6fd
Wahrith <warithtimi01@gmail.com>
2022-08-28 00:32:06 +0000

    yes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+
+
 def print_matrix_integer(matrix=[[]]):
-    for elm in matrix:
-        print(" ".join("{:d}".format(i) for i in elm))
+    """
+    A function that prints a matrix of integers.
+    """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
